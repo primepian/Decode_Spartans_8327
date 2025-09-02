26eb1c8595278cfe9055cdee6afde0a85ed23653
PrimePian <primepian52@gmail.com>
2025-09-02 15:05:54 -0600

    mmm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -5,7 +5,7 @@ import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-//
+//g
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants();
 
